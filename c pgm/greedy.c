132bdd6912222b040c5cca9b3d0b7c57eb27f146
hana12356 <156879623+hana12356@users.noreply.github.com>
2024-02-05 12:51:36 +0530

    Add files via upload

---
+++ c pgm/greedy.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<limits.h>
+#define N 5
+int matrixChainOrder(int p[],int n){
+    int m[N][N];
+    for(int i=1;i<n;i++)
+       m[i][i]=0;
+    for(int L=2;L<n;L++)
+       for(int i=1;i<n-L+1;i++){
+           int j=i+L-1;
+           m[i][j]=INT_MAX;
+           for(int k=i;k<=j;k++){
+               int q=m[i][k]+m[k+1][j]+p[i-1]*p[k]
+*p[j];
+               if(q<m[i][j])
+               m[i][j]=q;
+           }
+       }
+
+ return m[1][n-1];
+}
+
+int main()
+{
+  int arr[]={5,6,4,2,3};
+  int size=sizeof(arr)/sizeof(arr[0]);
+  printf("minimum number of multiplicaTION is %d\n",matrixChainOrder(arr,size));
+  return 0;
+}
+
