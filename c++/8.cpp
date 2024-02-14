b54af8648e316687b9a13eea63617fe496ee8e69
hana12356 <156879623+hana12356@users.noreply.github.com>
2024-02-14 20:25:40 +0530

    Add files via upload

---
+++ c++/8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+ fstream file; 
+ file.open("sample.txt",ios::out);
+ if(!file)
+ {
+ cout<<"Error in creating file!!!"<<endl;
+ }
+ cout<<"File created successfully."<<endl;
+ file<<"ABCD.";
+ file.close();
+ file.open("sample.txt",ios::in);
+ if(!file)
+ {
+ cout<<"Error in opening file!!!"<<endl;
+ } 
+ char ch;
+ cout<<"File content: ";
+ while(!file.eof())
+ {file>>ch;
+ cout<<ch;
+ }
+ file.close();
+ return 0;
+}
