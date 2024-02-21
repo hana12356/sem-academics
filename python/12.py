9e4d1465c0690c7ffd31f67e6e48d1b2e0a766f5
hana12356 <156879623+hana12356@users.noreply.github.com>
2024-02-21 23:28:16 +0530

    Update 12.py

--- python/12.py
+++ python/12.py
@@ -1,1 +1,16 @@
+test_tup = (5, 20, 3, 7, 6, 8)
+print("The original tuple is : " + str(test_tup))
+ 
+K = 2
+
+res = []
+test_tup = list(sorted(test_tup))
+
+for idx, val in enumerate(test_tup):
+	if idx < K or idx >= len(test_tup) - K:
+		res.append(val)
+res = tuple(res)
+
+# printing result 
+print("The extracted values : " + str(res)) 
 
