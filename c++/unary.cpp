99842e5d73af9d67dfb69b88c0d2d58e83c0cecd
hana12356 <156879623+hana12356@users.noreply.github.com>
2024-02-26 21:48:32 +0530

    Add files via upload

---
+++ c++/unary.cpp
@@ -0,0 +1,37 @@
+#include <iostream> 
+using namespace std; 
+ class Distance
+{ 
+public: 
+ int feet, inch; 
+ 
+ // Constructor to initialize 
+ // the object's value 
+ Distance(int f, int i) 
+ { 
+ this->feet = f; 
+ this->inch = i; 
+ } 
+ 
+ // Overloading(-) operator to 
+ // perform decrement operation 
+ // of Distance object 
+ void operator-() 
+ { 
+ feet--; 
+ inch--; 
+ cout << "\nFeet & Inches(Decrement): " << 
+ feet << "'" << inch; 
+ } 
+}; 
+ 
+// Driver Code 
+int main() 
+{ 
+ Distance d1(8, 9); 
+ 
+ // Use (-) unary operator by 
+ // single operand 
+ -d1; 
+ return 0; 
+}
