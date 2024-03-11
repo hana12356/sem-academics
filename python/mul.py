98762ad3451af464f182fd7329dc7b5147afafb9
hana12356 <156879623+hana12356@users.noreply.github.com>
2024-03-11 19:08:11 +0530

    Update mul.py

--- python/mul.py
+++ python/mul.py
@@ -8,3 +8,16 @@ num = 12
 # Iterate 10 times from i = 1 to 10
 for i in range(1, 11):
    print(num, 'x', i, '=', num*i)
+
+
+output:
+12 x 1 = 12
+12 x 2 = 24
+12 x 3 = 36
+12 x 4 = 48
+12 x 5 = 60
+12 x 6 = 72
+12 x 7 = 84
+12 x 8 = 96
+12 x 9 = 108
+12 x 10 = 120
